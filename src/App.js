865ffe6834917ac3f79e9bc0d0a5030dd8ac97f1
Joel <joelrzief@gmail.com>
2020-03-16 21:48:22 -0400

    Component for person added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const Person = ({ x, y, infected, dead, recovered }) => {
+  //should use styled components here
+  let strokeColor = 'rgb(146, 120, 226)';
+  let fillColor = 'white';
+
+  if (infected) {
+    strokeColor = 'rgb(246, 102, 64)';
+    fillColor = 'rgb(246, 102, 64)';
+  } else if (dead) {
+    strokeColor = 'rgbq(0, 0, 0, 0.5)';
+  } else if (recovered) {
+    strokeColor = 'rgb(146, 129, 227)';
+  }
+  return (
+    <circle
+      cx={x}
+      cy={y}
+      r="5"
+      style={{ fill: fillColor, stroke: strokeColor, strokeWidth: 2 }}
+    ></circle>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Visualizing the spread of viruses in a population</h1>
+      <svg
+        style={{
+          width: '100vw',
+          height: '100vh'
+        }}
+      >
+        <Person x={100} y={100} infected></Person>
+      </svg>
     </div>
   );
 }
