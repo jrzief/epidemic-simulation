628e5bc6ae80af77d3ea8d09fe2f5191ddbea6d2
Joel <joelrzief@gmail.com>
2020-03-17 14:32:02 -0400

    Putting a population on the page in a diamond shape

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import * as d3 from 'd3';
 
 const Person = ({ x, y, infected, dead, recovered }) => {
   //should use styled components here
@@ -23,8 +24,59 @@ const Person = ({ x, y, infected, dead, recovered }) => {
     ></circle>
   );
 };
+// generates a poulation oriented around cx, cy
+function createRow({ cx, cy, width }) {
+  ///fit as many as possible into row
+  const N = Math.floor(width / 15);
+  // point scale positions a row for us
+  const xScale = d3
+    .scalePoint()
+    .domain(d3.range(0, N))
+    .range([cx - width / 2, cx + width / 2]);
+
+  const row = d3.range(0, N).map(i => ({
+    x: xScale(i),
+    y: cy
+  }));
+
+  return row;
+}
+
+function createPopulation({ cx, cy, width, height }) {
+  const Nrows = Math.floor(height / 15);
+
+  const yScale = d3
+    .scalePoint()
+    .domain(d3.range(0, Nrows + 1))
+    .range([cy - height / 2, cy + height / 2]);
+
+  // should replace the shape here with a circle
+  const widthScale = d3
+    .scaleLinear()
+    .domain([0, Nrows / 2, Nrows])
+    .range([15, width, 15]);
+
+  const rows = d3
+    .range(0, Nrows + 1)
+    .map(i => createRow({ cx, cy: yScale(i), width: widthScale(i) }));
+
+  console.log('rows', rows);
+  //return rows;
+  let reducerow = rows.reduce((population, row) => [...population, ...row]);
+
+  //return rows;
+  console.log('reducerow', reducerow);
+  return reducerow;
+}
 
 function App() {
+  const population = createPopulation({
+    cx: 400,
+    cy: 200,
+    width: 400,
+    height: 300
+  });
+
   return (
     <div className="App">
       <h1>Visualizing the spread of viruses in a population</h1>
@@ -34,7 +86,9 @@ function App() {
           height: '100vh'
         }}
       >
-        <Person x={100} y={100} infected></Person>
+        {population.map(p => (
+          <Person x={p.x} y={p.y} infected />
+        ))}
       </svg>
     </div>
   );
